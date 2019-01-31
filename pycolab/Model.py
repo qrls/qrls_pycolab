1df0acda7121b022d15a5b645424326911ed4a22
Arun Kumar <arun.anthony.kumar@gmail.com>
2019-01-31 19:39:21 +0000

    Q-learning start

--- pycolab/Model.py
+++ pycolab/Model.py
@@ -1,6 +1,10 @@
 import random
+import numpy as np
 from time import sleep
 def AgentModel(Observation,actions,reward):
 	sleep(0.05)
-	action = random.choice(actions)
+	#action = random.choice(actions)
+	Observation_size = Observation.size()
+	action_size = len(actions)
+	qtable = np.zeros((Observation_size, action_size))
 	return action
